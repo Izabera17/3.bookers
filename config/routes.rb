be2d43f0d33d6396e1835e27fb514d26812614c7
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2022-10-03 06:17:10 +0000

    [Start]3-12 投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  get 'top' => 'homes#top', as: 'top_homes'
+  get 'books/index'  => 'books#index', as: 'index_books'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
