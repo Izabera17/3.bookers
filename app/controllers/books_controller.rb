f5d37ce75e2365bfad4833f848f1b05b01158391
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2022-10-04 07:34:42 +0000

    [Start]3-12 投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,27 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.new
   end
 
   def index
     @book = Book.all
   end
-
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+  
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  get 'top' => 'homes#top', as: 'top_homes'
+ 
   get 'books/new'
+  post 'book/new'
   get 'books/show'
   get 'books/edit'
   get 'books'  => 'books#index', as: 'index_books'
+  post 'create' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+  
